699838b4ee43e214783aa04ffa4f5659a8c49698
Abdur Rahman <ar8058276@gmail.com>
2026-06-10 18:10:30 -0500

    Half practice of Day-4

---
+++ JavaScript/Day4/script.js
@@ -0,0 +1,60 @@
+// let userInput = "   HeLLo JaVAScript   ";
+// let cleanInput = userInput.trim().toLowerCase();
+// console.log(cleanInput);
+
+// let email = " USER@EXAMPLE.COM";
+// let newEmail = email.trim().toLowerCase();
+// console.log(newEmail);
+
+// let password = "  MyP@awOrd123";
+// let securePassword = password.trim().toUpperCase().slice(0, 9);
+// console.log(securePassword);
+
+// let fullName = "Alexander Hamilton";
+// let firstName = fullName.slice(0, 9);
+// let lastName = fullName.slice(10);
+// console.log(firstName, lastName);
+
+// let phone = "6285457964";
+// let areaCode = phone.slice(0, 3);
+// let lastFour = phone.slice(6);
+// console.log(`Area code : ${areaCode}, last four :${lastFour}`);
+
+// let announcement = "The event will happen on Monday and Monday only";
+// let updatedAnnouncement = announcement.replace("Monday", "Friday");
+// console.log(announcement, updatedAnnouncement);
+
+// let colors = ["red", "blue", "green", "yellow"];
+// console.log(colors[colors.length - 1]); //last array element using .length
+// console.log("The total elements are ", colors.length);
+
+// let playList = [];
+// playList.push("Song 1");
+// playList.push("Song 2");
+// console.log(playList.pop());
+// playList.unshift("Song 0");
+// console.log(playList);
+
+// let fruits = ["apple", "banana", "mango", "orange", "banana"];
+// console.log(fruits.indexOf("apple"));
+// console.log(fruits.indexOf("banana"));
+// console.log(fruits.indexOf("grape")); return -1
+
+// let inventory = ["laptop", "mouse", "keyboard"];
+// console.log(inventory.includes("mouse"));
+// console.log(inventory.includes("printer"));
+
+// let morningClasses = ["Math", "English", "Science"];
+// let afternoonClasses = ["History", "Art", "PE"];
+// let fullSchedule = morningClasses + " " + afternoonClasses;
+// console.log(fullSchedule);
+
+// let originalArr = [1, 2, 3, 4, 5];
+// let reversedArr = originalArr.reverse();
+// console.log("Original array is:", originalArr);
+// console.log("Reversed array is:", reversedArr);
+
+//splice(starting element,delet element, add element)
+// let months = ["jan", "feb", "mar", "may", "jun"];
+// months.splice(3, 0, "apr");
+// console.log(months);
