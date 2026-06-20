939b3d89c1d4b1a7acadeed669ad5940a0cf1501
Abdur Rahman <ar8058276@gmail.com>
2026-06-19 22:27:32 -0500

    Day 10 of JavaScript practice

---
+++ JavaScript/Day10/script.js
@@ -0,0 +1,33 @@
+// console.dir(document);
+
+// let box = document.getElementById("Box");
+// console.log(box);
+
+// let car = document.getElementById("car");
+// console.log(car);
+
+// let msg = document.getElementsByClassName("msg");
+// console.log(msg);
+
+// let para = document.getElementsByTagName("p");
+// console.log(para);
+
+// let oldImages = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < oldImages.length; i++) {
+//   console.log(oldImages[i]);
+// }
+
+// let boxLinks = document.querySelectorAll(".box a");
+// console.log(boxLinks);
+
+// let para = document.querySelector(".description");
+// console.log(para.innerText);
+
+// let firstPara = document.querySelector("p");
+// console.log(firstPara.innerText);
+
+// let para = document.querySelector(".description");
+// console.log(para.textContent);
+
+// let heading = document.querySelector("#heading1");
+// heading.innerHTML = "<u>The Solar System</u>";
